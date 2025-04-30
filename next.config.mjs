77875d6010ea1967e2e1a7d3914a1d4c885fe4c3
yogooneko <brrrrism@gmail.com>
2025-04-30 17:30:50 +0900

    chore: disable next/image optimization for static <img>

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
-// 필요한 다른 import 구문이 있다면 여기에 둡니다.
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-
   images: {
-    unoptimized: true, // ← 이것만 추가하면 돼!
+    unoptimized: true, // HTML <img> 태그 쓸 때 최적화 끄기
   },
 };
 
